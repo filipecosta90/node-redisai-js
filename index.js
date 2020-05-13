da13f9bcd2995ba155981cdd579f8bfb6dbfbd23
filipecosta90 <filipecosta.90@gmail.com>
2020-05-13 13:44:34 +0100

    [add] added binding for ai.dagrun_ro

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ^v0.9 Commands
-const redisaiCommands = ["ai.tensorset", "ai.tensorget", "ai.modelset", "ai.modelget", "ai.modeldel", "ai.modelrun", "ai._modelscan", "ai.scriptset", "ai.scriptget", "ai.scriptdel", "ai.scriptrun", "ai.dagrun", "ai.info", "ai.config"];
+const redisaiCommands = ["ai.tensorset", "ai.tensorget", "ai.modelset", "ai.modelget", "ai.modeldel", "ai.modelrun", "ai._modelscan", "ai.scriptset", "ai.scriptget", "ai.scriptdel", "ai.scriptrun", "ai.dagrun", "ai.dagrun_ro", "ai.info", "ai.config"];
 
 function addRedisAICommands(redis) {
     redisaiCommands.forEach(redis.addCommand);
